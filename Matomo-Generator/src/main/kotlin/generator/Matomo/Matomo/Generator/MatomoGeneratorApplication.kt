8abfcd7c365b5a8b533fe13b0e35dbabe74d6d84
EArt1 <emanuel.artinger@gmail.com>
2023-09-14 14:26:58 +0200

    start of the architecture

---
+++ Matomo-Generator/src/main/kotlin/generator/Matomo/Matomo/Generator/MatomoGeneratorApplication.kt
@@ -0,0 +1,11 @@
+package generator.Matomo.Matomo.Generator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatomoGeneratorApplication
+
+fun main(args: Array<String>) {
+	runApplication<MatomoGeneratorApplication>(*args)
+}
---
+++ Matomo-Generator/src/test/kotlin/generator/Matomo/Matomo/Generator/MatomoGeneratorApplicationTests.kt
@@ -0,0 +1,13 @@
+package generator.Matomo.Matomo.Generator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MatomoGeneratorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
